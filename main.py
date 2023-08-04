28c16f5e738bb2cd79733cc77106aee941e3d6ed
Pedro Lopez Mareque <pedro.lopez@clarity.ai>
2023-08-04 16:07:25 +0200

    feat: enable cors

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.delivery.api.v1.status_router import status_router
 from src.delivery.api.v1.capitals.play_capitals_router import play_capitals_router
@@ -7,6 +8,21 @@ from src.delivery.api.v1.countries.play_countries_router import play_countries_r
 from src.delivery.api.v1.countries.solve_countries_router import solve_countries_router
 
 app = FastAPI()
+
+origins = [
+    "https://capitalsui.onrender.com",
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:8081",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(status_router)
 app.include_router(play_capitals_router)
